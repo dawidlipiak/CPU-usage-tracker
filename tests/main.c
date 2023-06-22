92c597c2f14b658953b5d9cc1cc8d25e99d8b383
dawidlipiak <dawidlipiak@gmail.com>
2023-06-22 17:39:23 +0100

    Tests added for buffer

---
+++ tests/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>      
+#include "buffer_test.h"
+
+int main(void) {
+
+    printf("Buffer Testing Started...\n");
+    test_buffer();
+    printf("Buffer Testing Finished!\n");
+
+    return 0;
+}
